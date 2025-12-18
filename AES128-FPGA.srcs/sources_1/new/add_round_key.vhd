55119f69f7d4db7e3d4387b312fb089d332bba3e
LucasPlacentino <23436953+LucasPlacentino@users.noreply.github.com>
2025-12-18 20:49:21 +0100

    Update add_round_key.vhd

--- AES128-FPGA.srcs/sources_1/new/add_round_key.vhd
+++ AES128-FPGA.srcs/sources_1/new/add_round_key.vhd
@@ -16,12 +16,39 @@ use IEEE.STD_LOGIC_1164.ALL;
 use work.matrix_type_pkg.ALL;
 
 entity add_round_key is
---  Port ( );
+    port (
+        matrix_in: in byte_matrix_t; -- input to AddRoundKey
+        key: in byte_matrix_t; -- key for AddRoundKey
+        matrix_out: out byte_matrix_t -- output block
+    );
 end add_round_key;
 
 architecture Behavioral of add_round_key is
 
+    -- bitwise XOR between input matrix and key
+
+
+    --signal temp_matrix: byte_matrix4;
+
 begin
 
+    -- process(matrix_in, key)
+    -- begin
+    --     for i in 0 to 3 loop
+    --         for j in 0 to 3 loop
+    --             temp_matrix(i, j) <= matrix_in(i, j) xor key(i, j);
+    --         end loop;
+    --     end loop;
+    -- end process;
+
+    -- matrix_out <= temp_matrix;
+
+
+    --without a process:
+    gen_rows: for i in 0 to 3 generate
+        gen_cols: for j in 0 to 3 generate
+            matrix_out(i, j) <= matrix_in(i, j) xor key(i, j);
+        end generate;
+    end generate;
 
 end Behavioral;
